60e851528a5a0809130c9a16ba74bead9c24ea28
hudhud <hadihorani@hotmail.com>
2019-06-15 01:24:26 +0200

    Created MainActivity based on mockup. Intents are missing the proper destination

--- app/src/main/java/dtu/app/visito/MainActivity.java
+++ app/src/main/java/dtu/app/visito/MainActivity.java
@@ -1,13 +1,56 @@
 package dtu.app.visito;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends Activity {
+
+    private Button topAttractionsBtn, weatherBtn, currencyBtn;
+    private TextView title;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        title = findViewById(R.id.title);
+        topAttractionsBtn = findViewById(R.id.topAttractionsBtn);
+        weatherBtn = findViewById(R.id.weatherBtn);
+        currencyBtn = findViewById(R.id.currencyBtn);
+
+        Typeface tf = Typeface.createFromAsset(getAssets(), "fonts/COMIC.TTF");
+        title.setTypeface(tf);
+
+        topAttractionsBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        weatherBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        currencyBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
