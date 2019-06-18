b49b78226445d5a04d76d0799b973545bd1096b8
hudhud <hadihorani@hotmail.com>
2019-06-18 18:12:52 +0200

    Images can now be loaded from URL

--- app/src/main/java/dtu/app/visito/CustomListAdapter.java
+++ app/src/main/java/dtu/app/visito/CustomListAdapter.java
@@ -5,25 +5,26 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.squareup.picasso.Picasso;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+
+import static android.content.ContentValues.TAG;
 
 public class CustomListAdapter extends ArrayAdapter<String> {
 
@@ -35,7 +36,6 @@ public class CustomListAdapter extends ArrayAdapter<String> {
 
     public CustomListAdapter(Activity context, ArrayList<String> lstAttractions, ArrayList<String> lstAttractionIcons) {
         super(context, R.layout.list_layout, lstAttractions);
-        // TODO Auto-generated constructor stub
 
         this.context=context;
         this.lstAttractions=lstAttractions;
@@ -50,8 +50,23 @@ public class CustomListAdapter extends ArrayAdapter<String> {
         final ImageView imageView = (ImageView) rowView.findViewById(R.id.ivIcon);
 
         txtTitle.setText(lstAttractions.get(position));
-        imageView.setImageDrawable(null);
+
+        loadImageFromURL(imageView, "https://firebasestorage.googleapis.com/v0/b/visito-4b840.appspot.com/o/Amalienborg.jpg?alt=media&token=f58b2531-852c-4c39-8328-77fdaa0bf0d2");
+
+        //imageView.setImageDrawable(null);
 
         return rowView;
+    }
+
+    public void loadImageFromURL(ImageView iv, String imgURL) {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .permitAll()
+                .build());
+        try {
+            URL url = new URL(imgURL);
+            iv.setImageBitmap(BitmapFactory.decodeStream((InputStream)url.getContent()));
+        } catch (IOException e) {
+           System.out.println(e.getMessage());
+        }
     }
 }
